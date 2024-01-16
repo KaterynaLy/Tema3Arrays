0aaf9a3e6908ac2afdaf7861f1d76c87bc4eb628
Kateryna Lysenko <132901241+KaterynaLy@users.noreply.github.com>
2024-01-16 12:59:12 +0100

    ej4

---
+++ ej4.php
@@ -0,0 +1,7 @@
+<?php
+
+$infoPersonal = array("nombre" => "Kateryna", "edad" => 37, "email" => "kat@gmail.com", "comidaFavorita" => "helado");
+echo "nombre" . ": " . $infoPersonal['nombre'] . ", ";
+echo 'edad' . ": " . $infoPersonal['edad'] . ", ";
+echo 'email' . ": " . $infoPersonal['email'] . ", ";
+echo 'comidaFavorita' . ": " . $infoPersonal['comidaFavorita'];
